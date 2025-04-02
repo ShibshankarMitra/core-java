e3175128debee0a15479147e3f8731f5067eae63
RAVEN <shibu.rkmv@gmail.com>
2025-04-02 12:02:06 +0530

    modified formatting

--- src/Arrays/ArrContiguousSum.java
+++ src/Arrays/ArrContiguousSum.java
@@ -1,29 +1,24 @@
 package Arrays;
 
 public class ArrContiguousSum {
-	
-	int contigousSumOfArr(int[] arr) {
-		
-		int max_so_far=arr[0];
-		int max_ending_here=0;
-		
-		for(int i=0;i<arr.length;i++) {
-			max_ending_here= max_ending_here+arr[i];
-			if(max_ending_here<0)
-				max_ending_here=0;
-			else if(max_so_far<max_ending_here)
-			{
-				max_so_far=max_ending_here;
-			}
-		}
-		return max_so_far;
-	}
 
-	public static void main(String[] args) {
-		int[] arr= {4, -2, -3, 4, -1, -2, 1, 5, -3};
-		ArrContiguousSum ags= new ArrContiguousSum();
-		System.out.println(ags.contigousSumOfArr(arr));
+    int contiguousSumOfArr(int[] arr) {
+        int max_so_far = arr[0];
+        int max_ending_here = 0;
+        for (int j : arr) {
+            max_ending_here = max_ending_here + j;
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+            else if (max_so_far < max_ending_here) {
+                max_so_far = max_ending_here;
+            }
+        }
+        return max_so_far;
+    }
 
-	}
-
+    public static void main(String[] args) {
+        int[] arr = {4, -2, -3, 4, -1, -2, 1, 5, -3};
+        ArrContiguousSum ags = new ArrContiguousSum();
+        System.out.println(ags.contiguousSumOfArr(arr));
+    }
 }
